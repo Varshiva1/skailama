40e9ea7b2bce4ae8848e477449fdcfc777c6395f
Shivam Varun <86642542+Varshiva1@users.noreply.github.com>
2024-09-05 00:28:14 +0530

    updated vercel json

--- server/index.js
+++ server/index.js
@@ -1,21 +1,20 @@
-// index.js
 import express from 'express';
 import connectDB from './db.js';
 import cors from 'cors'; 
 import userRoutes from './routes/userRoutes.js';
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+const app = express(); 
 
-const app = express();
+
 connectDB();
-
 app.use(express.json());
 app.use(cors()); 
- 
+
 app.use('/api', userRoutes);
-// app.use('/api', discussionRoutes);
-console.log('Routes registered');
-const PORT = 5000;
+app.get("/", (req, res) => res.send("Express on Vercel"));
+
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-module.exports = app;
+export default app;  
