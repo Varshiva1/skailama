155dfb22d1d00aab98004d40f301040fdd1dd7ad
Shivam Varun <86642542+Varshiva1@users.noreply.github.com>
2024-09-05 11:08:13 +0530

    fix: protected route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,41 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 import Login from "./pages/login/Login";
 import "./App.css";
 import ProjectsList from "./pages/projects-list";
 import ProjectDetails from "./pages/project-details";
+
+const ProtectedRoute = ({ children }) => {
+  const user = useSelector((state) => state.auth.data);
+
+  if (!user || !user.token) {
+    return <Navigate to="/" replace />;
+  }
+  return children;
+};
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/projects" element={<ProjectsList />} />
-        <Route path="/:filename/:tab" element={<ProjectDetails />} />
+        <Route
+          path="/projects"
+          element={
+            <ProtectedRoute>
+              <ProjectsList />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/:filename/:tab"
+          element={
+            <ProtectedRoute>
+              <ProjectDetails />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </Router>
   );
